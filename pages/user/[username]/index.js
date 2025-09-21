4e6154ee480a3f4cd86e6be795b6a12acf181ca6
[Wasim Akhtar Khan] <wasimakhtarkhan2003@gmail.com>
2025-09-21 16:14:07 +0530

    Learnt various Router Functions like router.push and router.replace and router.reload and also learnt to use client for accessing any react hooks

--- pages/user/[username]/index.js
+++ pages/user/[username]/index.js
@@ -1,9 +1,31 @@
+'use client';
 import {useRouter} from 'next/router'
+import { useEffect } from 'react';
+
+const user ={
+        userList:null
+    };
 const Home=()=>{
     const router=useRouter();
+    useEffect(()=>{
+        if(user.userList==null){
+            router.push('/');
+        }
+    },[router,user.userList]);
     const{username}=router.query;
+    const handleClick=()=>{
+        router.push({
+            pathname:"/user/[username]/settings", // it is pathname not path
+            query:{username} // must be an object
+        })
+    }
     return <>
     <h1>This is the Index Page of {username}</h1>
+    <button onClick={handleClick}>Go to Settings</button>
+    <br />
+    <button onClick={()=>router.replace(`/`)}>Go to HOME !</button>
+    <br />
+    <button onClick={()=>router.reload()}> RELOAD </button>
     </>
 };
 export default Home ;
